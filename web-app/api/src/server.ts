6467be841e1db2c3be2712b24ac487d089617a7a
paulo <paulofvilarimda@gmail.com>
2024-06-29 03:02:14 -0300

    adjust import routes

--- web-app/api/src/server.ts
+++ web-app/api/src/server.ts
@@ -1,5 +1,5 @@
 import fastify, { type FastifyInstance } from "fastify";
-import { userRoutes } from "./routes/user.routes.js";
+import { userRoutes } from "./routes/index.js";
 
 const app: FastifyInstance = fastify({
   logger: {
@@ -9,9 +9,14 @@ const app: FastifyInstance = fastify({
   },
 });
 
-app.register(userRoutes, { prefix: "/users" });
+try {
+  app.register(userRoutes, { prefix: "/users" });
 
-app.listen({
-  host: "0.0.0.0",
-  port: 3001,
-});
+  await app.listen({
+    host: "0.0.0.0",
+    port: 3001,
+  });
+} catch (err) {
+  console.log(err);
+  process.exit(1);
+}
