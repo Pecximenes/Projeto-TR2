42b4ae50fbd0e37ade0a1ffc5d26e5b8c39c49d0
paulo <paulofvilarimda@gmail.com>
2024-06-30 02:47:02 -0300

    gateways dashboard page

--- web-app/front/src/app/dashboard/layout.tsx
+++ web-app/front/src/app/dashboard/layout.tsx
@@ -5,7 +5,9 @@ export default function LayoutDashboard({ children }: React.PropsWithChildren) {
   return (
     <>
       <Navbar />
-      <main className="min-h-screen pt-14 sm:pt-16">{children}</main>
+      <main className="min-h-screen w-full px-8 pb-10 pt-14 sm:pt-16 md:px-24">
+        {children}
+      </main>
       <Footer />
     </>
   );
--- web-app/front/src/app/dashboard/page.tsx
+++ web-app/front/src/app/dashboard/page.tsx
@@ -1,5 +1,52 @@
+import { mockGateways } from "~/mocks/gateways";
+
+import { Button, buttonVariants } from "~/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import Link from "next/link";
+
 export default function DashboardPage() {
   return (
-    <main>Pagina para visualização do dashboard dos gateways e tanques</main>
+    <>
+      <section className="my-8 flex w-full items-center justify-between">
+        <span className="font-light">
+          Veja os Gateways da UnB e Clique em um Para Ver Mais
+        </span>
+        <Button>Adicionar Gateway</Button>
+      </section>
+      {!mockGateways?.length && <span>Crie um Gateway...</span>}
+      <div className="flex flex-col flex-wrap items-center justify-between md:flex-row md:items-start">
+        {mockGateways?.map((gateway) => (
+          <Card
+            key={gateway.id}
+            className="mt-2 w-80 shadow-md transition-shadow ease-in-out hover:shadow-lg md:w-[48%]"
+          >
+            <CardHeader>
+              <CardTitle>{gateway.name}</CardTitle>
+              <CardDescription>{gateway.description}</CardDescription>
+            </CardHeader>
+            <CardContent className="flex flex-col md:flex-row md:gap-2">
+              <p className="line-clamp-2">
+                Arduino de ID {gateway.arduinoId}, {gateway.address}
+              </p>
+            </CardContent>
+            <CardFooter>
+              <Link
+                href={`/dashboard/gateway/${gateway.id}`}
+                className={buttonVariants()}
+              >
+                Veja Mais
+              </Link>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </>
   );
 }
