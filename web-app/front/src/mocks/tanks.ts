be96e8ab6fda93d0c04e04e5ed4e910358186477
paulo <paulofvilarimda@gmail.com>
2024-06-30 03:38:36 -0300

    fix mock tank in front

--- web-app/front/src/mocks/tanks.ts
+++ web-app/front/src/mocks/tanks.ts
@@ -1,51 +1,26 @@
-const mockTanks = [
-  {
-    id: "1",
-    arduinoId: 201,
-    name: "Tank 1",
-    address: "789 Oak St",
-    description: "First tank",
-    gateway: {
-      name: "Gateway 1",
-      arduinoId: 101,
-    },
-    levels: [
-      {
-        id: "1",
-        level: 50,
-        caughtAt: new Date("2022-01-01T10:00:00Z"),
-      },
-      {
-        id: "2",
-        level: 60,
-        caughtAt: new Date("2022-01-02T10:00:00Z"),
-      },
-    ],
+const mockTank = {
+  id: "1",
+  arduinoId: 201,
+  name: "Tank 1",
+  address: "789 Oak St",
+  description: "First tank",
+  gateway: {
+    name: "Gateway 1",
+    arduinoId: 101,
   },
-  {
-    id: "2",
-    arduinoId: 202,
-    name: "Tank 2",
-    address: "101 Pine St",
-    description: "Second tank",
-    gateway: {
-      name: "Gateway 2",
-      arduinoId: 102,
+  levels: [
+    {
+      id: "1",
+      level: 50,
+      caughtAt: new Date("2022-01-01T10:00:00Z"),
     },
-    levels: [
-      {
-        id: "3",
-        level: 70,
-        caughtAt: new Date("2022-01-03T10:00:00Z"),
-      },
-      {
-        id: "4",
-        level: 80,
-        caughtAt: new Date("2022-01-04T10:00:00Z"),
-      },
-    ],
-  },
-];
+    {
+      id: "2",
+      level: 60,
+      caughtAt: new Date("2022-01-02T10:00:00Z"),
+    },
+  ],
+};
 
 const mockTankCreate = {
   arduinoId: 203,
@@ -62,4 +37,4 @@ const mockTankUpdate = {
   gatewayId: "1",
 };
 
-export { mockTanks, mockTankCreate, mockTankUpdate };
+export { mockTank, mockTankCreate, mockTankUpdate };
